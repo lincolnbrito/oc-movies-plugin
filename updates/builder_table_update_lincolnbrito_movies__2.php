001dac4c74c42c1db5eca3c2e91f6ab50308df0a
Lincoln Brito <lincoln.sbrito@gmail.com>
2019-10-09 16:42:55 -0300

    add slug field

---
+++ updates/builder_table_update_lincolnbrito_movies__2.php
@@ -0,0 +1,23 @@
+<?php namespace LincolnBrito\Movies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateLincolnbritoMovies2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('lincolnbrito_movies_', function($table)
+        {
+            $table->string('slug')->nullable()->after('year');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('lincolnbrito_movies_', function($table)
+        {
+            $table->dropColumn('slug');
+        });
+    }
+}
