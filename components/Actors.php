8f1f267afa87755ab68615c9df1cf687a7e66b14
Lincoln Brito <lincoln.sbrito@gmail.com>
2019-10-14 17:05:42 -0300

    add options to sort and number of results

--- components/Actors.php
+++ components/Actors.php
@@ -16,6 +16,33 @@ class Actors extends ComponentBase
         ];
     }
 
+    public function defineProperties()
+    {
+        return [
+            'results' => [
+                'title' => 'Number of actors',
+                'description' => 'How many actors do you want to display?',
+                'default' => 0,
+                'validationPattern' => '^[0-9]+$',
+                'validationMessage' => 'Only numbers allowed.'
+            ],
+            'sortOrder' => [
+                'title' => 'Sort actors',
+                'description' => 'How to sort the actors list',
+                'type' => 'dropdown',
+                'default' => 'name asc',                
+            ]
+        ];
+    }
+
+    public function getSortOrderOptions()
+    {
+        return [
+            'name asc' => 'Name (ascending)',
+            'name desc' => 'Name (descending)'
+        ]; 
+    }
+
     public function onRun()
     {
         $this->actors = $this->loadActors();
@@ -23,6 +50,20 @@ class Actors extends ComponentBase
 
     protected function loadActors()
     {
-        return Actor::all();
+        $query =  Actor::all();
+
+        if( $this->property('sortOrder') == 'name asc') {
+            $query = $query->sortBy('name');
+        }
+
+        if( $this->property('sortOrder') == 'name desc') {
+            $query = $query->sortByDesc('name');
+        }
+
+        if( $this->property('results') > 0) {
+            $query = $query->take($this->property('results'));
+        }
+
+        return $query;
     }
 }
