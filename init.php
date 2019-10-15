d5251f3a7288080463202332f1b510b537344a60
Lincoln Brito <lincoln.sbrito@gmail.com>
2019-10-15 01:51:47 -0300

    create init file to event listener

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Event;
+
+// Event::listen('auth.login', function($event) { 
+//     dd('EVENTOOO');die();
+
+//  }, 10);
+
+//  Event::listen('mailer.send', function ($message, $view, $data) {
+//     \Log::info("$view has had content added to the message", $data, $message);
+// });
