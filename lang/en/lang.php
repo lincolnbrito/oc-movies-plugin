b107eeb7eeab20eb041b7ade99fafb36cebdba1f
Lincoln Brito <lincoln.sbrito@gmail.com>
2019-10-19 01:15:49 -0300

    add permissions

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,12 @@
 <?php return [
     'plugin' => [
         'name' => 'Movies',
-        'description' => 'A example plugin'
-    ]
+        'description' => 'A example plugin',
+    ],
+    'permission' => [
+        'manage_movies' => 'Manage Movies',
+        'manage_genres' => 'Manage Genres',
+        'manage_actors' => 'Manage Actors',
+        'delete_movies' => 'Delete Movies',
+    ],
 ];
