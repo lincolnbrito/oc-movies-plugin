2ab98eefe24bc99b45ca84f029bed8644881a0e5
Lincoln Brito <lincoln.sbrito@gmail.com>
2019-10-21 11:09:00 -0300

    relation manager to genres

--- controllers/Movies.php
+++ controllers/Movies.php
@@ -8,12 +8,14 @@ class Movies extends Controller
     public $implement = [        
         'Backend\Behaviors\ListController',        
         'Backend\Behaviors\FormController',        
-        //'Backend\Behaviors\ReorderController'    
+        //'Backend\Behaviors\ReorderController',
+        'Backend\Behaviors\RelationController'  
     ];
     
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
+    public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
     {
