667c1d15a19ed2fe5f05626a0fc1dac5b0b8b548
Lincoln Brito <lincoln.sbrito@gmail.com>
2019-10-20 23:39:42 -0300

    add published field to movies table, create new list columns

---
+++ updates/builder_table_update_lincolnbrito_movies__5.php
@@ -0,0 +1,23 @@
+<?php namespace LincolnBrito\Movies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateLincolnbritoMovies5 extends Migration
+{
+    public function up()
+    {
+        Schema::table('lincolnbrito_movies_', function($table)
+        {
+            $table->boolean('published')->nullable()->after('slug');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('lincolnbrito_movies_', function($table)
+        {
+            $table->dropColumn('published');
+        });
+    }
+}
