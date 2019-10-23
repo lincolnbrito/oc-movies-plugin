3d8cf382cf078d5dce22112c606d257f7191a7e3
Lincoln Brito <lincoln.sbrito@gmail.com>
2019-10-23 11:16:27 -0300

    create sitemap.xml

--- routes.php
+++ routes.php
@@ -1,5 +1,8 @@
 <?php
 use LincolnBrito\Movies\Models\Actor;
+use LincolnBrito\Movies\Models\Genre;
+use LincolnBrito\Movies\Models\Movie;
+
 Route::prefix('api/movies')->group(function(){
     Route::get('seed-actors', function() {
         
@@ -14,4 +17,15 @@ Route::prefix('api/movies')->group(function(){
 
         return 'Actors created';
     });
+});
+
+
+Route::get('sitemap.xml', function(){
+    $movies = Movie::all();
+    $genres = Genre::all();
+
+    return Response::view('lincolnbrito.movies::sitemap', [
+        'movies' => $movies,
+        'genres' => $genres
+    ])->header('Content-Type','text/xml');
 });
